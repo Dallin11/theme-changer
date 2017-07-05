8648156a9517a5de545f894cff3bb2282c0af15f
Your Name <you@example.com>
2017-07-05 15:04:30 -0600

    Finnished

--- src/App.js
+++ src/App.js
@@ -8,27 +8,50 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-  // constructor
+    constructor () {  
+      super();      // constructor
+   this.state = {
+  fontColor: 'black',
+  fontSize: 12,
+  fontFamily: 'monospace',
+  allowEdit: 'true'
+   };
+      
 
-  // updateColor
+   this.updateColor = this.updateColor.bind(this);
+   this.updateSize = this.updateSize.bind(this);
+   this.updateFamily = this.updateFamily.bind(this);
+   this.updateEditStatus = this.updateEditStatus.bind(this)
 
-  // updateSize
+  }
+  updateColor(val){   // updateColor
+  this.setState({ fontColor: val }); 
+  }
+  updateSize(val){
+    this.setState({ fontSize: val });
+  }                  // updateSize
+   updateFamily(val){
+     this.setState({ updateFamily: val });
+   }                 // updateFamily
 
-  // updateFamily
-
-  // updateEditStatus
+   updateEditStatus(val){
+     this.setState({ allowEdit: val })
+   }                 // updateEditStatus
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle update={this.updateEditStatus /* Render EditToggle */ } />
+          <ColorChanger update={this.updateColor /* Render ColorChanger */ } allowEdit={this.state.allowEdit} />
+          <SizeChanger update={ this.updateSize/* Render SizeChanger */ } allowEdit={this.state.allowEdit} />
+          <FamilyChanger update={this.updateFamily /* Render FamilyChanger */ } allowEdit={this.state.allowEdit} />
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+        <TextContainer
+         fontColor={ this.state.fontColor }
+         fontSize={ this.state.fontColor }
+         fontFamily={ this.state.fontFamily } />
         </div>
       </div>
     )
